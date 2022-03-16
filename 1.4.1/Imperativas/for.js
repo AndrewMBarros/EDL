0a299f15c731bd1b6c744a32687c716fc7602630
AndrewMBarros <49644696+AndrewMBarros@users.noreply.github.com>
2022-03-16 02:47:40 -0300

    linguagens imperativas

---
+++ 1.4.1/Imperativas/for.js
@@ -0,0 +1,3 @@
+for (let numero = 3; numero < 1000; numero = numero *2){
+    console.log("O número é: "+numero);
+}
---
+++ 1.4.1/Imperativas/switch.js
@@ -0,0 +1,19 @@
+let nome = "John Lennon";
+
+switch(nome){
+    case "John Lennon":
+        console.log("Você tem o nome do Beatle John");
+        break;
+    case "Paul McCartney":
+        console.log("Você tem o nome do Beatle Paul");
+        break;
+    case "George Harrison":
+        console.log("Você tem o nome do Beatle John");
+        break;
+    case "Ringo Starr":
+        console.log("Você tem o nome do Beatle Ringo");
+        break;
+    default:
+        console.log("Você não tem o nome de nenhum dos Beatles");
+        break;
+}
