e41a7e771b31c5ccc6de2627c8fdd9fd94f3be7d
AndrewMBarros <49644696+AndrewMBarros@users.noreply.github.com>
2022-03-21 01:16:08 -0300

    Add files via upload

---
+++ T2/verificarnumero.php
@@ -0,0 +1,13 @@
+<?php 
+
+$numero = 50;
+
+if($numero == 10):
+    echo "O número digitado é igual a 10";
+elseif($numero <=9):
+    echo "O número digitado é menor ou igual a 9";
+else:
+    echo"O número digitado é diferente de 10";
+endif;
+
+echo "<hr>";
